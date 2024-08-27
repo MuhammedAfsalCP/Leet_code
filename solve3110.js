921dd6d195f5f855c85b9a0317d41d3fe5ab21e3
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2024-08-27 10:17:52 +0530

    new

---
+++ solve3110.js
@@ -0,0 +1,24 @@
+var scoreOfString = function(s) {
+   let a=s.split("")
+   let sum=[]
+   let r=[]
+   let w
+   
+   for(let i=0;i<a.length;i++){
+     sum.push(a[i].charCodeAt())
+   }
+   for(let i=0;i<sum.length;i++){
+     w=0;
+    if(i<sum.length-1){
+        w=Math.abs(sum[i]-sum[i+1])
+        r.push(w)
+    
+    
+   }
+}
+let y=r.reduce((acc,val)=>acc+=val,0)
+
+    console.log(y);
+    
+};
+scoreOfString("hello")
