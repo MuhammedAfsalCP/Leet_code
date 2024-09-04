3a1007e3e1516d612e5ab0a1efebcbdf0432c7a2
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2024-09-04 10:25:40 +0530

    new

--- solve2000.js
+++ solve2000.js
@@ -1,7 +1,7 @@
 var reversePrefix = function(word, ch) {
     let c =word.indexOf(ch)
-   let a=[...wor]
-   let d=a.splice(0,c+1).reverse().joi("")
+   let a=[...word]
+   let d=a.splice(0,c+1).reverse().join("")
    a=a.join("")
     return (d+a)
 };
