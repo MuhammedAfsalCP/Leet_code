f7436d1f81e6d1fa2f510d18132313453b14aff7
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2024-09-13 08:40:41 +0530

    new

---
+++ solve34.js
@@ -0,0 +1,18 @@
+var searchRange = function(nums, target) {
+    let b=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==target){
+          b.push(i)
+        }
+    }
+    if(b.length==0){
+        return [-1,-1]
+    }else{
+        if(b.length==1){
+            b.push(b[0])
+            return b
+        }else{
+            return [b[0],b[b.length-1]]
+        }
+    }
+};
