ae59d0be251e4f1f5d657c4ba7462ed1e702c436
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2024-10-11 09:47:25 +0530

    new

---
+++ solve1089.js
@@ -0,0 +1,14 @@
+var duplicateZeros = function(arr) {
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]==0){
+        for(let j=arr.length-1;j>i;j--){
+           arr[j]=arr[j-1]
+        }
+        if(i+1!=arr.length){
+            arr[i+1]=0
+        i++
+        }
+      }
+       
+    }
+};
