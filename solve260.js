0f2f3a76340940f296fa436ae08983232eae3e33
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2024-10-18 09:28:38 +0530

    new

---
+++ solve260.js
@@ -0,0 +1,19 @@
+var singleNumber = function(nums) {
+    let arr=[]
+      for (let i = 0; i < nums.length; i++) {
+        let count=0
+        if (nums[i] != -100) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    nums[j] = -100
+                    count++
+                }
+            }
+            if (count==0) {
+                arr.push(nums[i])
+            }
+
+        }
+    }
+    return arr
+};
