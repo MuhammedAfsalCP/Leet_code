2937c14879e534b716fd511de3cac132f2d7ed0e
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2024-11-13 08:56:51 +0530

    new

---
+++ solve283.js
@@ -0,0 +1,16 @@
+var moveZeroes = function(nums) {
+    let a=nums.filter((val)=>val!=0)
+    let c=a.length
+    let b=nums.length;
+    let d=[]
+    for(let i=0;i<b;i++){
+        if(i<c){
+            d.push(a[i])
+        }else{
+            d.push(0)
+        }
+    }
+    nums.length=0
+    nums.push(...d)
+    console.log(nums)
+};
