3f6c5201951a7d37c3e88abcc77f7eef535573a4
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2024-11-16 08:39:23 +0530

    new

---
+++ solve219.js
@@ -0,0 +1,16 @@
+var containsNearbyDuplicate = function(nums, k) {
+    let seen = new Set(); // Track the numbers within the current window
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])) {
+            return true; 
+        }
+        
+        seen.add(nums[i]);
+        if (seen.size > k) {
+            seen.delete(nums[i - k]);
+        }
+    }
+    
+    return false; 
+};
