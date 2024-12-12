1d996a766524922d3bb0d6dce06ad03a0a4544fa
Muhammed Afsal CP <168629450+MuhammedAfsalCP@users.noreply.github.com>
2024-12-12 08:23:00 +0530

    Time: 0 ms (100%), Space: 51.1 MB (78.46%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let count=0
+    let sum=0
+    let miss=0
+   for(let i=1;i<=nums.length;i++){
+    count=count+i
+   }
+   for(let i=0;i<nums.length;i++){
+    sum+=nums[i]
+   }
+   miss=count-sum
+   return miss
+};
