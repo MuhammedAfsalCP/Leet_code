6cd511c221a082a24f1b1bbcd637e4f794c7dde5
Muhammed Afsal CP <168629450+MuhammedAfsalCP@users.noreply.github.com>
2024-12-17 10:03:10 +0530

    Time: 8 ms (38.73%), Space: 50.9 MB (64.82%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -6,23 +6,9 @@
 var findTheDifference = function(s, t) {
   s=s.split("").sort()
   t=t.split("").sort()
-  let b=[]
-  let c;
-  let e
-  if(s.length>t.length){
-        c=s
-        e=t
-  }else{
-    c=t
-    e=s
-  }
-  let j=0
-  for (let i=0;i<c.length;i++){
-    if(c[i]!=e[i]){
-        return c[i]
-        
+  for (let i=0;i<t.length;i++){
+    if(s[i]!=t[i]){
+        return t[i]  
     }
   }
- 
-
 };
