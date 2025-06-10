78a7414aba2f08a412482d2a82393098cba9a5b3
Muhammed Afsal CP <168629450+MuhammedAfsalCP@users.noreply.github.com>
2025-06-10 12:17:05 +0530

    Time: 0 ms (100%), Space: 15.8 MB (68.79%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,29 +5,23 @@
 #         self.next = next
 class Solution(object):
     def oddEvenList(self, head):
-        curr=head
-        odd=[]
-        even=[]
-        i=0
-        while curr:
-            if i%2==0:
-                odd.append(curr.val)
-            else:
-                even.append(curr.val)
-            curr=curr.next
-            i+=1
-        lenofodd=len(odd)
-        i=0
-        k=0
-        curr=head
-        while curr:
-            if i<lenofodd:
-                curr.val=odd[i]
-            else:
-                curr.val=even[k]
-                k+=1
-            i+=1
-            curr=curr.next
+        if not head or not head.next:
+            return head
+        odd=head
+        even=odd.next
+        even_head=even
+        # while odd and even.next :
+        #     odd.next=even.next
+        #     odd=odd.next
+        #     even.next=odd.next
+        #     even=odd.next
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next=even_head
         return head
+
             
         
