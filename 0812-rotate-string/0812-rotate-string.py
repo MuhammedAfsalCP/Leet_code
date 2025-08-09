0779647cb47a545b7eeee846d9fbcfa8f0a9580e
Muhammed Afsal CP <168629450+MuhammedAfsalCP@users.noreply.github.com>
2025-08-09 15:27:21 +0530

    Time: 0 ms (100%), Space: 12.3 MB (80.46%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        if len(s)==len(goal) and goal in (s+s):
+            return True
+        else:
+            return False
+
+        
