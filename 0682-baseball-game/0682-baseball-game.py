2f103a744cd4d331953d13fae1be4dc3522e76c8
Muhammed Afsal CP <168629450+MuhammedAfsalCP@users.noreply.github.com>
2025-08-15 12:04:11 +0530

    Time: 3 ms (50.58%), Space: 12.4 MB (96.17%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,25 @@
+from functools import reduce
+class Solution(object):
+    def calPoints(self, operations):
+        arr=[]
+        sum1=0
+        for x in range(len(operations)):
+            if operations[x].lstrip('-').isdigit():
+                # print(operations[x])
+                arr.append(int(operations[x]))
+            elif operations[x].isalpha():
+                if operations[x]=="C":
+                    arr.pop()
+                if operations[x]=="D":
+                    # print("HI")
+                    arr.append(arr[-1]*2)
+            elif operations[x]=="+":
+                # print(sum(arr))
+                arr.append(arr[-2]+arr[-1])
+                sum1=sum(arr)
+            
+        return sum(arr)
+            
+            
+
+        
